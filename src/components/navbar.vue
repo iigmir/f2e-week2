cf21dae2b0becf752dc78e6a27af27259515b0c4
iigmir <roc120j@gmail.com>
2018-06-16 05:03:50 +0800

    dev nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,28 @@
 <template>
     <nav>
-        Navbar
+        <div>
+            <p> Location </p>
+            <select name="location" id="nav-location">
+                <option value="1">fdns</option>
+                <option value="2">fdns</option>
+                <option value="3">fdns</option>
+            </select>
+        </div>
+        <div>
+            <p> Categories </p>
+            <label>
+                <input type="checkbox" name="all" id=""> All
+            </label>
+            <label>
+                <input type="checkbox" name="c1" id=""> /
+            </label>
+            <label>
+                <input type="checkbox" name="q1" id=""> /
+            </label>
+            <label>
+                <input type="checkbox" name="w1" id=""> /
+            </label>
+        </div>
     </nav>
 </template>
 
