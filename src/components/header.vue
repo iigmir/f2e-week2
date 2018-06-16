22f4fe26c1b81b6498c62ab7b2fc5b017e51e67a
iigmir <roc120j@gmail.com>
2018-06-16 15:41:52 +0800

    navbar set

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,7 +4,7 @@
             <h1>{{ msg }}</h1>
         </div>
         <div class="search">
-            <i class="fas fa-search fa-2x"></i>
+            <i class="fas fa-search"></i>
             <input type="text" placeholder="Explore your own activities" />
         </div>
     </header>
@@ -24,20 +24,30 @@ header
     background: #7828B4;
     color: #FFFFFF;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    // grid-template-rows: 40px 4em 40px;
-    grid-gap: 1rem;
-    grid-auto-rows: minmax(10rem, auto);
+    grid-template-columns: repeat(3, 1fr);
+    // grid-gap: 1rem;
+    grid-auto-rows: minmax(7rem, auto);
 
     .title, .search { margin: auto; }
 
+    .title
+    {
+        grid-column: 1;
+        h1
+        {
+            font-family: "Optima";
+            padding-left: 3rem;
+        }
+    }
+
     .search
     {
+        grid-column: 2 / 3;
         input
         {
             border: 0;
-            width: 30rem;
-            height: 5rem;
+            width: 20rem;
+            height: 3rem;
             font-size: 1.2rem;
             color: rgba(255,255,255,0.50);
             background-color: rgba( 0, 0, 0, 0 );
@@ -48,6 +58,7 @@ header
         i
         {
             transform: translate(1.5rem,5px);
+            font-size: 1.5rem;
         }
     }
 }
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,24 +3,39 @@
         <div class="bar">
             <p> Location </p>
             <select name="location" id="nav-location">
-                <option value="1">fdns</option>
-                <option value="2">fdns</option>
-                <option value="3">fdns</option>
+                <option value="0">All</option>
+                <option value="1">B ares</option>
+                <option value="2">C area</option>
             </select>
         </div>
         <div class="bar">
+            <p> Date </p>
+            <label class="txt">
+                <span>From</span>
+                <input type="date" name="foo">
+            </label>
+            <label class="txt">
+                <span>To</span>
+                <input type="date" name="bar">
+            </label>
+        </div>
+        <div class="bar">
             <p> Categories </p>
-            <label>
-                <input type="checkbox" name="all" id=""> All
+            <label class="checkbox">
+                <input type="checkbox" name="all">
+                <span>All</span>
             </label>
-            <label>
-                <input type="checkbox" name="c1" id=""> /
+            <label class="checkbox">
+                <input type="checkbox" name="c1"> 
+                <span>siuhdg</span>
             </label>
-            <label>
-                <input type="checkbox" name="q1" id=""> /
+            <label class="checkbox">
+                <input type="checkbox" name="q1"> 
+                <span>ewoitq</span>
             </label>
-            <label>
-                <input type="checkbox" name="w1" id=""> /
+            <label class="checkbox">
+                <input type="checkbox" name="w1"> 
+                <span>cbns,d,n</span>
             </label>
         </div>
     </nav>
@@ -38,13 +53,14 @@ export default {
 <style lang="scss" scoped>
 nav
 {
-    width: 80%;
-    margin-left: 5rem;
+    width: 300px;
+    margin-left: 8rem;
     text-align: left;
     background: #EBEBEB;
     .bar
     {
         padding: 2rem;
+        border-bottom: 1px solid #D7D7D7;
         p
         {
             font-family: "Roboto";
@@ -57,6 +73,23 @@ nav
             border: 0;
             padding: 1rem;
             font-size: 16px;
+        }
+        label
+        {
+            display: block;
+            font-size: 16px;
+            color: #000000;
+            &.txt
+            {
+                display: flex;
+                padding: 2px;
+                justify-content: space-between;
+                & > * { flex-basis: 100%; }
+                input
+                {
+                    padding: 4px;
+                }
+            }
         }
     }
 }
